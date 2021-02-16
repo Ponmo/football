f73e0aa1f579c6ca433e01eb1873aef71a7e6b61
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-02-15 21:27:46 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $(".card").on({
     mouseenter: function() {
-      $(this).css("box-shadow", "0 16px 16px 0 rgba(0,0,0,0.5)");
+      $(this).css("box-shadow", "0 16px 32px 0 rgba(0,0,0,0.4)");
     },
     mouseleave: function(){
-      $(this).css("box-shadow", "0 8px 8px 0 rgba(0,0,0,0.3)");
+      $(this).css("box-shadow", "0 8px 16px 0 rgba(0,0,0,0.2)");
     },
     click: function(){
       $(this).css("");
