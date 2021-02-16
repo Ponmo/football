0045bb2a3be4111c86b4a2592adb1eb0fde717dd
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-02-15 21:13:33 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".card").on("mouseover", (function() {
+  $(".card").on("mouseover", function() {
     $(this).css("box-shadow", "0 32px 64px 0 rgba(0,0,0,0.2)");
   });
 });
