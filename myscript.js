fd5c0bbfa80ed9e4bc4605c2a65dc452d6d02f7c
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-02-15 22:04:06 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  var q;
   $(".card").on({
     mouseenter: function() {
       $(this).css("box-shadow", "0 16px 32px 0 rgba(0,0,0,0.4)");
@@ -7,7 +8,11 @@ $(document).ready(function() {
       $(this).css("box-shadow", "0 8px 16px 0 rgba(0,0,0,0.2)");
     },
     click: function(){
+      if(q!=0) {
+        $(".card").css("transform", "none");
+      }
       $(this).css("transform", "rotateY(180deg)");
+      q=1;
     }
   });
 });
